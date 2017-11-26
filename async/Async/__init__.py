1e5a81fd85f0cc7642b273a662ec16e7075e4ef5
theBuzzyCoder <buzykarthik@gmail.com>
2017-11-26 08:31:23 +0530

    updates

--- async/Async/__init__.py
+++ async/Async/__init__.py
@@ -1,39 +1,15 @@
-
 import math
+import asyncio
 from itertools import islice
 
 
-def fibonacci(number):
-    i = 2
-    a = 0
-    b = 1
-    fibonacci_series = [a, b]
-    while i < number:
-        a, b = b, a + b
-        i += 1
-        fibonacci_series.append(b)
-    return fibonacci_series
+async def print_reminder(interval, future):
+    while not future.done():
+        print("This is a reminder.")
+        await asyncio.sleep(interval)
 
 
-def async_fibonacci():
-    a = 0
-    yield a
-    b = 1
-    while True:
-        yield b
-        a, b = b, a + b
-
-
-def lucas(number):
-    i, a, b = 2, 2, 1
-    lucas_sequence = [a, b]
-    while i < number:
-        i += 1
-        a, b = b, a + b
-        lucas_sequence.append(b)
-
-
-def async_lucas():
+def lucas():
     a = 2
     yield a
     b = 1
@@ -42,64 +18,27 @@ def async_lucas():
         a, b = b, a + b
 
 
-def async_get_all_odd_numbers():
-    i = 1
-    while True:
-        yield i
-        i += 2
-
-
-def is_prime(number):
-    if number < 2:
+async def is_prime(number):
+    if number <= 2:
         return False
-    for i in range(3, int(math.sqrt(number)) + 1):
+    for i in range(2, int(math.sqrt(number)) + 1):
         if number % i == 0:
             break
-        yield
+        await asyncio.sleep(0)
     else:
         return True
     return False
 
 
-class Future(object):
-    def __init__(self, generator):
-        self.gen = generator
-        self.output = None
-
-    def resolve(self):
-        return self.gen.next()
+async def print_results(iterable, async_function, task_name, future):
+    for item in iterable:
+        if await async_function(item):
+            print("Found: %s in %s" % (item, task_name))
+    future.set_result("Done!")
 
 
-class Task(object):
-    def __init__(self):
-        self.ready_task = list()
-        self.success_task = list()
-        self.failed_task = list()
-        self.suspended_task = list()
-
-    def add_task(self, generator):
-        future = Future(generator)
-        self.ready_task.append(future)
-
-    def resume_task(self):
-        future = self.suspended_task.pop(0)
-        self.ready_task.append(future)
-
-    def run_to_complete(self):
-        while self.ready_task:
-            try:
-                future = self.ready_task.pop(0)
-                yielded = future.resolve()
-            except StopIteration as e:
-                future.output = e.value
-                print("Task successfully finished. Output: %s" % e.value)
-                self.success_task.append(future)
-            except Exception as e:
-                print("Task Failed: %s" % str(e))
-                self.failed_task.append(future)
-            else:
-                assert yielded is None
-                self.suspended_task.append(future)
-                if (not self.ready_task) and self.suspended_task:
-                    future = self.suspended_task.pop(0)
-                    self.ready_task.append(future)
+loop = asyncio.get_event_loop()
+future = loop.create_future()
+loop.create_task(print_reminder(3, future))
+loop.create_task(print_results(islice(lucas(), 30, 40), is_prime, 'task1', future))
+loop.run_until_complete(future)
---
+++ async/Async/my_async.py
@@ -0,0 +1,91 @@
+import time
+import math
+import asyncio
+
+
+class Task(object):
+    def __init__(self, generator):
+        self.start = time.time()
+        self.gen = generator
+        self.total = None
+
+
+class Scheduler(object):
+    def __init__(self):
+        self.ready_task = list()
+        self.success_task = list()
+        self.failed_task = list()
+
+    def add_task(self, generator):
+        task = Task(generator)
+        self.ready_task.append(task)
+
+    def run_to_completion(self):
+        while self.ready_task:
+            try:
+                task = self.ready_task.pop(0)
+                yielded = next(task.gen)
+            except StopIteration:
+                print("Task successfully finished.")
+                task.total = time.time() - task.start
+                self.success_task.append(task)
+            except Exception as e:
+                print("Task Failed: %s" % str(e))
+                self.failed_task.append(task)
+            else:
+                assert yielded is None
+                self.ready_task.append(task)
+
+
+def print_reminder(message, interval):
+    i = 0
+    while True:
+        print(message)
+        yield from async_sleep(interval)
+        i += 1
+
+
+def async_sleep(interval):
+    start = time.time()
+    end = start + interval
+    while True:
+        yield
+        if end <= time.time():
+            break
+
+
+def lucas():
+    a = 2
+    yield a
+    b = 1
+    while True:
+        yield b
+        a, b = b, a + b
+
+
+def is_prime(number):
+    if number <= 2:
+        return False
+    for i in range(2, int(math.sqrt(number)) + 1):
+        if number % i == 0:
+            break
+        # yield from asyncio.sleep(0)
+    else:
+        return True
+    return False
+
+
+def print_results(iterable, async_function, task_name):
+    for item in iterable:
+        # match = yield from async_function(item)
+        # if match:
+        #     print("Found: %s in %s" % (item, task_name))
+        if async_function(item):
+            print("Found: %s in %s" % (item, task_name))
+        yield
+
+
+s = Scheduler()
+s.add_task(print_reminder('async message', 3))
+s.add_task(print_results(lucas(), is_prime, 'task1'))
+s.run_to_completion()
